08a639b9634e53226fa10ced7f17508366f4cb7c
Matt0912 <matt.meades01@gmail.com>
2020-03-10 19:53:53 +0000

    Census complete - 50% done cw1

--- dbvm/CW1/census.sql
+++ dbvm/CW1/census.sql
@@ -22,40 +22,112 @@ occId = 2 AND gender = 0;
 
 -- !question1
 
--- Place your answers to question 1 here, between the
--- "-- !question1" and "-- !question2" lines. You can delete
--- this comment.
+SELECT data FROM Statistic
+WHERE WardId = 'E05001975' AND gender = 1 AND occId = 7;
 
 -- !question2
 
--- Place your answer to question 2 here. Delete this comment.
+SELECT SUM(data) FROM Statistic
+WHERE WardId = 'E05000697' AND occId = 5;
 
 -- !question3
 
--- Place your answer to question 3 here. Delete this comment.
+SELECT SUM(data) AS num_people, Occupation.name AS occ_class FROM Statistic
+JOIN Occupation ON Statistic.occId = Occupation.id
+WHERE WardId = 'E05008884'
+GROUP BY Occupation.name;
 
 -- !question4
 
--- Place your answer to question 4 here. Delete this comment.
+SELECT SUM(data) AS 'working population', WardId,
+Ward.name AS Ward, County.name AS County FROM Statistic
+JOIN Ward ON Statistic.Wardid=Ward.Code
+JOIN County ON County.Code=Ward.parent
+GROUP BY WardId
+ORDER BY SUM(data) ASC
+LIMIT 1;
 
 -- !question5
 
--- Place your answer to question 5 here. Delete this comment.
+WITH OverThousand AS
+  (SELECT SUM(data) AS 'working population', WardId AS ID,
+  Ward.name AS Ward, County.name AS County FROM Statistic
+  JOIN Ward ON Statistic.Wardid=Ward.Code
+  JOIN County ON County.Code=Ward.parent
+  GROUP BY WardId
+  HAVING SUM(data) >= 1000)
+  SELECT COUNT(*) AS 'Wards Over 1000' FROM OverThousand;
+
 
 -- !question6
 
--- Place your answer to question 6 here. Delete this comment.
+WITH RegionSizes AS
+  (SELECT SUM(Statistic.data) AS working_population, WardId AS ID,
+  Ward.name AS Ward, County.name AS County, Region.code AS Region FROM Statistic
+  JOIN Ward ON Statistic.Wardid=Ward.Code
+  JOIN County ON County.Code=Ward.parent
+  JOIN Region ON County.parent=Region.code
+  GROUP BY WardId
+  ORDER BY Region ASC)
+  SELECT Region.name AS name, AVG(RegionSizes.working_population) AS avg_size FROM RegionSizes
+  JOIN Region ON RegionSizes.Region=Region.code
+  GROUP BY Region.name;
 
 -- !question7
 
--- Place your answer to question 7 here. Delete this comment.
+SELECT County.name AS CLU, Occupation.name AS Occupation, (CASE WHEN
+gender = 1 THEN "female" ELSE "male" END) AS gender, SUM(data) AS N FROM Statistic
+JOIN Ward ON Statistic.Wardid=Ward.Code
+JOIN County ON County.Code=Ward.parent
+JOIN Region ON County.parent=Region.code
+JOIN Occupation ON Statistic.occId=Occupation.id
+WHERE Region.code = 'E12000002'
+GROUP BY County.name, Occupation, gender
+HAVING SUM(data) >= 10000
+ORDER BY N ASC;
 
 -- !question8
 
--- Place your answer to question 8 here. Delete this comment.
+WITH Managers AS
+  (SELECT Region.name AS Region, Occupation.name AS Occupation, (CASE WHEN
+  gender = 1 THEN "female" ELSE "male" END) AS gender, SUM(data) AS N FROM Statistic
+  JOIN Ward ON Statistic.Wardid=Ward.Code
+  JOIN County ON County.Code=Ward.parent
+  JOIN Region ON County.parent=Region.code
+  JOIN Occupation ON Statistic.occId=Occupation.id
+  WHERE Occupation.id = 1
+  GROUP BY Region, gender
+  ORDER BY Region ASC),
+Almost AS
+  (SELECT Region, MAX(CASE WHEN gender = "male" THEN N ELSE NULL END) AS MaleManagers,
+  MAX(CASE WHEN gender = "female" THEN N ELSE NULL END) AS FemaleManagers FROM Managers
+  GROUP BY Region)
+  SELECT Region, MaleManagers, FemaleManagers,
+  CONCAT(ROUND(FemaleManagers/(MaleManagers + FemaleManagers)*100,2), '%') AS FemaleProportion FROM Almost
+  ORDER BY FemaleProportion ASC;
+
 
 -- !question9
 
--- Place your answer to question 9 here. Delete this comment.
-
+WITH RegionSizes AS
+  (SELECT SUM(Statistic.data) AS working_population, WardId AS ID,
+  Ward.name AS Ward, County.code AS County, Region.code AS Region FROM Statistic
+  JOIN Ward ON Statistic.Wardid=Ward.Code
+  JOIN County ON County.Code=Ward.parent
+  JOIN Region ON County.parent=Region.code
+  GROUP BY WardId
+  ORDER BY Region ASC),
+AllData AS
+  (SELECT SUM(Statistic.data) AS working_population, WardId AS ID,
+  Ward.name AS Ward FROM Statistic
+  JOIN Ward ON Statistic.Wardid=Ward.Code
+  GROUP BY WardId)
+  SELECT Region.name AS name, AVG(RegionSizes.working_population) AS avg_size FROM RegionSizes
+  JOIN Region ON RegionSizes.Region=Region.code
+  GROUP BY Region.name
+  UNION
+  SELECT 'England', AVG(RegionSizes.working_population) FROM RegionSizes
+  UNION
+  SELECT 'All', AVG(AllData.working_population) FROM AllData;
+  
 -- !end
